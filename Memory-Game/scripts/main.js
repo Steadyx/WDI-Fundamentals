2727d95367468b109936d0bfeab27a12bffdd00d
edward <edwardlanekemp@gmai.com>
2016-06-12 21:09:23 -0400

    Added score and fixed several issues

--- Memory-Game/scripts/main.js
+++ Memory-Game/scripts/main.js
@@ -1,8 +1,12 @@
 var cards = ["queen", "queen", "king", "king"],
   board = document.getElementById('board'),
+  scoreBoard = document.querySelector('.scorecard'),
+  currentScore = document.createElement('p'),
+  winText = document.querySelector('.win-match'),
   cardElement = document.createElement('div'),
   button = document.getElementById('clear'),
   cardSelect = document.querySelectorAll('.cards'),
+  score = 0;
   cardsInPlay = [];
 
 
@@ -19,73 +23,77 @@ function createBoard() {
 }
 
 function isMatch() {
-  var cardSelect = document.querySelectorAll('.cards');
+  var cardSelect = document.querySelectorAll('.cards'),
+      winScore = document.getElementById('score');
 
   if (cardsInPlay[0] === cardsInPlay[1]) {
+    winText.style.display = 'flex';
+     score += 1;
+    winScore.innerHTML = 'Your score is: ' + score;
 
+    setTimeout(function() {
+      winText.style.display = 'none';
+    }, 500);
   } else {
-    console.log('sorry, try again');
+    score -= 1;
+    winScore.innerHTML = 'Your score is: ' + score;
+
   }
 
 }
-function resetBoard(){
+
+function resetBoard() {
   var button = document.getElementById('clear');
+  button.addEventListener('click', function() {
+    var cardSelect = document.querySelectorAll('.cards');
+    winScore = document.getElementById('score');
 
-      button.addEventListener('click', function(){
-        var cardSelect = document.querySelectorAll('.cards');
+    console.log('clicked');
 
-        console.log('clicked');
+    if (cardsInPlay.length >= 0) {
+      for (var i = 0; i < cardSelect.length; i++) {
+        cardSelect[i].innerHTML = '';
+        winText.style.display = 'none';
+        winScore.innerHTML = '';
+        score = 0;
+      }
 
-        if(cardsInPlay.length >= 0){
-
-           cardSelect[0].innerHTML = '';
-           cardSelect[1].innerHTML = '';
-           cardSelect[2].innerHTML = '';
-           cardSelect[3].innerHTML = '';
-
-        }else {
-          console.log('must have atleast two cards in play');
-        }
-      });
+    } else {
+      console.log('must have atleast two cards in play');
+    }
+  });
 }
+
 function isTwoCards(el) {
-      var congrats = document.createElement('div'),
-          cardSelect = document.querySelectorAll('.cards');
+  var congrats = document.createElement('div'),
+    cardSelect = document.querySelectorAll('.cards');
 
   // add card to array of cards in play
   // 'this' hasn't been covered in this prework, but
   // for now, just know it gives you access to the card the user clicked on
   cardsInPlay.push(this.getAttribute('data-card'));
 
+  if (this.getAttribute('data-card') === 'king') {
+
+    this.innerHTML = '<img src="Images/king.png" class="cardImg">';
+
+  } else {
+
+    this.innerHTML = '<img src="Images/queen.png" class="cardImg">';
+
+  }
   // if you have two cards in play check for a match
   if (cardsInPlay.length === 2) {
-
-    for (var i = 0; i < cards.length; i++) {
-
-      if (cardsInPlay[0] === cards[i] || cardsInPlay[1] === cards[i]) {
-
-        cardSelect[i].innerHTML = '<img src="Images/' + cards[i] + '.png" class="cardImg">';
-
-      }if (cardsInPlay[0] !== cards[i] || cardsInPlay[1] !== cards[i]) {
-
-        cardSelect[i].innerHTML = '';
-
-      } else {
-
-        console.log('guess again!');
-
-      }
-
-    }
-
-    // pass the cardsInPlay as an argument to isMatch function
     isMatch(cardsInPlay);
-    // clear cards in play array for next try
     cardsInPlay = [];
-    resetBoard();
+
   }
 
+  // pass the cardsInPlay as an argument to isMatch function
+  // clear cards in play array for next try
+  resetBoard();
 }
+
 
 
 createBoard();
